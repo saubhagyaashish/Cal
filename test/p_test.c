dd9066602843aa817f93c83ce321b25d2b4976c4
99002463 <71491422+99002463@users.noreply.github.com>
2020-09-19 11:46:44 +0530

    Update p_test.c
    test cases changes

--- test/p_test.c
+++ test/p_test.c
@@ -1,41 +1,72 @@
 
-#include "unity.h"
-#include "p_cal.h"
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
 
+/* Modify these two lines according to the project */
+#include <p_main..h>
+#define PROJECT_NAME    "Calculator"
 
+/* Prototypes for all the test functions */
+void test_add(void);
+void test_subtract(void);
+void test_multiply(void);
+void test_divide(void);
 
-/* Required by the unity test framework */
-void setUp(){}
-/* Required by the unity test framework */
-void tearDown(){}
-
-void test_calculator_add(void)
-{
-  TEST_ASSERT_EQUAL(0, calculator(0,0,0));
-  TEST_ASSERT_EQUAL(1, calculator(6,-5,0));
-    TEST_ASSERT_EQUAL(20, calculator(4,5,2));
+/* Start of the application test */
+int main() {
+/* Note: Do not edit START*/
+  /*Initialize and setup the Test Framework */
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+/* Note: Do not edit END */
   
+  
+  /* Add your test functions in this format for testing*/
+  CU_add_test(suite, "add", test_add);
+  CU_add_test(suite, "subtract", test_subtract);
+  CU_add_test(suite, "multiply", test_multiply);
+  CU_add_test(suite, "divide", test_divide);
+
+
+/* Note: Do not edit START*/
+  /* Setup Test Framework to output the result to Screen */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  
+  /* run the unit test framework*/
+  CU_basic_run_tests();
+  
+  /* Cleaning the Resources used by Unit test framework */
+  CU_cleanup_registry();
+/* Note: Do not edit END */
+  return 0;
 }
-void test_calculator_sub(void)
-{
-  TEST_ASSERT_EQUAL(-7, calculator(2,9,1));
-  TEST_ASSERT_EQUAL(7, calculator(9,2,1));
-}
-void test_multi(void)
-{
-  TEST_ASSERT_EQUAL(-25, calculator(-5,5,2));
-  TEST_ASSERT_EQUAL(0, matrix(0));
+
+/* Write all the test functions */ 
+void test_add(void) {
+  CU_ASSERT(30 == addition(10, 20));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1500 == addition(750, 7500));
 }
 
-int main(void)
-{
+void test_subtract(void) {
+  CU_ASSERT(-3 == subtracttion(0, 3));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1 == subtracttion(1000, 900));
+}
 
-  UNITY_BEGIN();
+void test_multiply(void) {
+  CU_ASSERT(0 == multiplication(1, 0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(2 == multiplication(2, 5));
+}
 
-/* Run Test functions */
-  RUN_TEST(test_calculator_add);
-  RUN_TEST(test_calculator_sub);
-  RUN_TEST(test_multi);
-
-  return UNITY_END();
+void test_divide(void) {
+  CU_ASSERT(0 == division(1, 0));
+  
+  /* Dummy fail*/
+  CU_ASSERT(3 == division(2, 2));
 }
