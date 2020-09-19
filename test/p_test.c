59d2eb5be00b1bb2e011974f5976a69b72af3b55
99002463 <71491422+99002463@users.noreply.github.com>
2020-09-19 13:35:32 +0530

    Update p_test.c
    test cases added

--- test/p_test.c
+++ test/p_test.c
@@ -11,22 +11,28 @@ void tearDown(){}
 
 void test_calculator_add(void)
 {
-
+TEST_ASSERT_EQUAL(9,calculator(4,5,0));
   TEST_ASSERT_EQUAL(1,calculator(6,-5,0));
-  TEST_ASSERT_EQUAL(20,calculator(4,5,2));
-  TEST_ASSERT_EQUAL(9,calculator(4,5,0));
   TEST_ASSERT_EQUAL(100,calculator(45,55,0));
+    TEST_ASSERT_EQUAL(0,matrix(0));
   
 }
 void test_calculator_sub(void)
 {
-  TEST_ASSERT_EQUAL(-7,calculator(2,9,1));
+
   TEST_ASSERT_EQUAL(7,calculator(9,2,1));
+    TEST_ASSERT_EQUAL(43,calculator(45,2,1));
+    TEST_ASSERT_EQUAL(-7,calculator(58,65,1));
+  
 }
 void test_multi(void)
 {
-  TEST_ASSERT_EQUAL(-25,calculator(-5,5,2));
-  TEST_ASSERT_EQUAL(0,matrix(0));
+  TEST_ASSERT_EQUAL(-49,calculator(-7,7,2));
+    TEST_ASSERT_EQUAL(72,calculator(9,8,2));
+      TEST_ASSERT_EQUAL(150,calculator(10,15,2));
+      TEST_ASSERT_EQUAL(250,calculator(5,50,2));
+  
+
 }
 
 void test_div(void)
