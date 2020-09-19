dc3e209a602d51f48bc4f0dd5c323cacab892b71
99002463 <71491422+99002463@users.noreply.github.com>
2020-09-19 13:28:56 +0530

    Update p_cal.h

--- inc/p_cal.h
+++ inc/p_cal.h
@@ -19,11 +19,11 @@
 * @param[in] choice of operator 0:add,1:sub,2:multiply,3:divide
 * @return int out of number1(choice)number2
 */
-int calci(int number,int number2,int choice);
-int add(int x, int y);
-int subtract(int x, int y);
-int multiply(int x, int y);
-int divide(int x, int y);
+int calculator(int number,int number2,int choice);
+int addition(int x, int y);
+int subtraction(int x, int y);
+int multiplication(int x, int y);
+int division(int x, int y);
 int power(int num1,int num);
 int root(int num1,int num);
 int logarithm(int num1,int num);
@@ -42,11 +42,11 @@ float sine_fun(float num1);
 float cos_fun(float num1);
 float sec_fun(float num1);
 
+int matrix();
 
 int matrix_sub();
-int matrix();
+
 int matrixs_sum();
-int matrix_mul();
 
 
-#endif /* #ifndef __FACTORIAL_H__ */
+#endif
