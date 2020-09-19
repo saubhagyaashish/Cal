45e65dd9125d33aa08526393a1ce42b137d175f5
99002463 <71491422+99002463@users.noreply.github.com>
2020-09-19 09:27:07 +0530

    Create p_test.c
    unit test framework added 
    reference from stepIn mini project by Mr. Bharat

---
+++ test/p_test.c
@@ -0,0 +1,39 @@
+
+#include "unity.h"
+#include "p_cal.h"
+
+
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+void test_calculator_add(void)
+{
+  TEST_ASSERT_EQUAL(20, calculator(4,5,2));
+  TEST_ASSERT_EQUAL(1, calcultor(6,-5,0));
+}
+void test_calculator_sub(void)
+{
+  TEST_ASSERT_EQUAL(-7, calculator(2,9,1));
+  TEST_ASSERT_EQUAL(7, calculator(9,2,1));
+}
+void test_multi(void)
+{
+  TEST_ASSERT_EQUAL(-25, calculator(-5,5,2));
+  TEST_ASSERT_EQUAL(0, matrix(0));
+}
+
+int main(void)
+{
+
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_calculator_add);
+  RUN_TEST(test_calculator_sub);
+  RUN_TEST(test_multi);
+
+  return UNITY_END();
+}
